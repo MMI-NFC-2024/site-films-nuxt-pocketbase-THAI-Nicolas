3c888d94e7fa3f0f442964ee2b6c9f7aa342e416
THAI-Nicolas <nicolas.thai@edu.univ-fcomte.fr>
2026-03-02 14:16:59 +0100

    Code de base avec type root

---
+++ app/layouts/default.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { nuxtCtx } from "nuxt/kit";
+</script>
+
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink to="/abonnements">Abonnements</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/calendrier">Calendrier</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/depenses">Dépenses</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+    <slot />
+  </div>
+</template>
---
+++ app/pages/abonnements.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template>
+  <h1>Page abonnements</h1>
+</template>
